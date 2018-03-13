851cad522a2df81a76bc43b63b2f9253375b57bd
git1 <eng.se.faaaziz@gmail.com>
2018-03-13 17:21:33 +0100

    Display success message

--- src/app/http-error-handler.service.ts
+++ src/app/http-error-handler.service.ts
@@ -4,8 +4,6 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
-//import { MessageService } from './message.service';
-
 /** Type of the handleError function returned by HttpErrorHandler.createHandleError */
 export type HandleError =
   <T> (operation?: string, result?: T) => (error: HttpErrorResponse) => Observable<T>;
@@ -19,13 +17,7 @@ export class HttpErrorHandler {
   createHandleError = (serviceName = '') => <T>
     (operation = 'operation', result = {} as T) => this.handleError(serviceName, operation, result);
 
-  /**
-   * Returns a function that handles Http operation failures.
-   * This error handler lets the app continue to run as if no error occurred.
-   * @param serviceName = name of the data service that attempted the operation
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
+ 
   handleError<T> (serviceName = '', operation = 'operation', result = {} as T) {
 
     return (error: HttpErrorResponse): Observable<T> => {
@@ -35,9 +27,6 @@ export class HttpErrorHandler {
       const message = (error.error instanceof ErrorEvent) ?
         error.error.message :
        `server returned code ${error.status} with body "${error.error}"`;
-
-      // TODO: better job of transforming error for user consumption
-     // this.messageService.add(`${serviceName}: ${operation} failed: ${message}`);
 
       // Let the app keep running by returning a safe result.
       return of( result );
